59ec1d5fc80d71745b93715eb3d14f3a7ff88071
Julian <40327956+Archi657@users.noreply.github.com>
2023-08-13 09:19:18 -0500

    built app

--- src/common/reducers/youtube/youtube-selectors.ts
+++
@@ -1,5 +0,0 @@
-import { RootState } from '../../../store';
-
-const selectYoutube = () => (state:RootState) => state.youtube;
-
-export default selectYoutube;
--- src/common/reducers/youtube/youtube-types.ts
+++
@@ -1,7 +0,0 @@
-export type YouTubeChannelData = {
-  loaded: boolean;
-  name: string;
-  subscriberCount: string;
-  videoCount: string,
-  viewCount: string,
-};
